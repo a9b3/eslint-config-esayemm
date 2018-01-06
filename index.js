febd9c4e0b5b58e4811eafff72c830e720b6556c
Sam Lau <esayemm@gmail.com>
2018-01-06 11:13:07 -0800

    add prettier eslint config

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: ['esayemm'],
-  extends: ['esayemm/configs/defaults', 'esayemm/configs/ecmascript6'],
+  extends: [
+    'esayemm/configs/defaults',
+    'esayemm/configs/ecmascript6',
+    'plugin:prettier/recommended',
+  ],
   rules: {
     'esayemm/align-imports': 2,
     'esayemm/sort-imports': 2,
--- react.js
+++ react.js
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: ['react'],
-  extends: ['esayemm', 'plugin:react/recommended'],
+  extends: [
+    'esayemm',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
   rules: {
     'react/no-did-mount-set-state': 0,
     'react/react-in-jsx-scope': 0,
